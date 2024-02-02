f77b0fa0fdd98c037190b10422052bcbf35bad6b
Tyler <t.mains515@hotmail.com>
2024-02-01 23:26:59 -0800

    hero screen done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import theBasics from "./TheBasics"
+export default function Hero(){
+    
+    return (
+        <div className='flex flex-col w-screen h-screen justify-center items-center gap-24 bg-slate-100'>
+            <h1 className="text-5xl italic font-medium">CV-Builder</h1>
+            <div className="flex flex-col w-1/3 h-1/4 shadow-lg rounded-2xl bg-white justify-evenly items-center">
+                <h3 className="text-2xl italic">The premier resume building application</h3>
+                <button onClick={theBasics} className="h-1/6 w-1/3 bg-blue-400 text-white rounded-md hover:bg-blue-500 font-medium text-xl">Get Started!</button>
+            </div>
+        </div>
+    )
+}
