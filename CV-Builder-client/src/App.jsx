bc4941ec4b7a2e6ef056d236c6db96ac87b7edd7
Morningstar515 <tmains515@hotmail.com>
2024-05-18 16:09:02 -0700

    backend outline done

--- CV-Builder-client/src/App.jsx
+++ CV-Builder-client/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Hero from './components/Hero'
 import TheBasics from './components/TheBasics'
+import axios from 'axios'
 import './App.css'
 
 function App() {
 
+
+const fetchAPI = async () => {
+	const res = await axios.get("http://localhost:5000/hello")
+	console.log(res)
+}
+
+useEffect(()=>{
+	fetchAPI()
+},[])
 	const [state,setState] = useState(<Hero change = {handleState}/>);
+
 	function handleState(next){
 		setState(next)
 	}
+
+
+
 	return (
 		<div className='flex flex-col w-screen h-screen justify-center items-center gap-4 bg-slate-100'>
 			{state}
+
 		</div>
+		
 	)
 }
 
--- CV-Builder-server/server.py
+++ CV-Builder-server/server.py
@@ -1,8 +1,28 @@
 import mysql.connector
+from flask import Flask, jsonify;
+from flask_cors import CORS, cross_origin
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd='qtip515', database='CVBuilder')
-mycursor = mydb.cursor()
-mycursor.execute("select Templates from TemplateTable where keyid='test'")
+app = Flask(__name__)
+cors = CORS(app, origins='*')
 
-for i in mycursor:
-    print(mycursor)
+@app.route("/hello", methods=['GET'])
+def hello():
+    return jsonify(
+        {
+            "hellos": [
+                'hi',
+                'hola'
+            ]
+        }
+    )
+
+def returnTemplate(templateName):
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd='qtip515', database='CVBuilder')
+    mycursor = mydb.cursor()
+    mycursor.execute("select Templates from TemplateTable where keyid={templateName}")
+    for i in mycursor:
+        return i
+    
+
+if __name__ =="__main__":
+    app.run(port=5000, debug=True)
